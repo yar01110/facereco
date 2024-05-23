428a10bef18aaab932665f6ba705ea9539374574
nidhal karchoud <114624188+yar01110@users.noreply.github.com>
2024-05-24 00:26:13 +0100

    Create webcam.py
    .

---
+++ webcam.py
@@ -0,0 +1,45 @@
+import cv2
+from threading import Thread
+import requests 
+import json 
+import base64
+
+
+  
+class Stream:
+  
+    def __init__(self):
+        self.video_capture = cv2.VideoCapture(0)
+        self.current_frame = self.video_capture
+      
+    # create thread for capturing images
+    def frameResponse(self,frame,host="localhost",port=5000):
+      headers = {
+          'content-type': 'image/jpeg',
+          # 'Accept':'text/plain'
+      }
+      try:
+          response = requests.post(url="http://localhost:5000/",data=frame,headers=headers)
+      except KeyError:
+          print("Connection Error")
+    def start(self):
+        
+        Thread(target=self._update_frame, args=()).start()
+  
+    def _update_frame(self):
+
+        while(True):
+            try:
+
+                self.current_frame = self.video_capture.read()[1]
+
+              
+                encodedFrame =cv2.imencode('.jpg',self.current_frame)[1].tobytes()
+
+                self.frameResponse(encodedFrame)
+
+                cv2.imshow('VIDEO',self.current_frame)
+                print(f"${self.current_frame.shape}")
+                cv2.waitKey(1)
+            except KeyError:
+                print("Connection Error")
